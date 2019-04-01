cd16e4b985fc4eebe96bb215b9f888893ba184f0
jkoprcina <koprcina.josip@gmail.com>
2019-04-01 19:00:13 +0200

    Almost done

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
 let body = document.querySelector("body");
+createFirstPage();
 
 //Generating the html and css needed to make the login/register page
 function createFirstPage() {
-  body.innerHTML += `<div class="login-registration">
+  body.innerHTML = 
+  `<div class="login-registration">
     <nav class="login-registration__nav">
-      <span class="login-registration__nav__label" id="login">Login</span>
-      <span class="login-registration__nav__label" id="register">Registration</span>
+      <span class="login-registration__nav__label" id="login">Login page</span>
+      <span class="login-registration__nav__label" id="register">Registration page</span>
     </nav>
-    <form class="login-registration__form">
-      <label class="login-registration__form__label"
-        >Username</label>
+    <form class="login-registration__form left">
+      <label>Username</label>
       <input
         type="text"
-        class="login-registration__form__input"
-        id="username"/>
-      <label class="login-registration__form__label"
-        >Password</label>
+        class="input-div__form__input"
+        id="username"
+        placeholder="Enter username.."/>
+      <label>Password</label>
       <input
         type="password"
-        class="login-registration__form__input"
-        id="password"/>
-      <label class="login-registration__form__label"
-        id="confirmPasswordLabel">Confirm password</label>
+        class="input-div__form__input"
+        id="password"
+        placeholder="Enter password.."/>
+      <label id="confirmPasswordLabel">Confirm password</label>
       <input
         type="password"
-        class="login-registration__form__input"
-        id="confirmPassword"/>
+        class="input-div__form__input"
+        id="confirmPassword"
+        placeholder="Repeat password.."/>
     </form>
-    <button class="login-registration__button">Register</button>
+    <button class="input-div__form__button">Register</button>
   </div>`;
 }
 
@@ -37,7 +39,7 @@ let isPageLoginOrRegistration = true;
 //Deciding the first page is a login page and making it into one
 function login() {
   if (isPageLoginOrRegistration) {
-    document.querySelector(".login-registration__button").innerHTML = "Login";
+    document.querySelector(".input-div__form__button").innerHTML = "Login";
     let form = document.querySelector(".login-registration__form");
     form.removeChild(document.querySelector("#confirmPassword"));
     form.removeChild(document.querySelector("#confirmPasswordLabel"));
@@ -48,15 +50,16 @@ function login() {
 //Deciding the first page is a registration page and and making it into one
 function register() {
   if (!isPageLoginOrRegistration) {
-    document.querySelector(".login-registration__button").innerHTML =
+    document.querySelector(".input-div__form__button").innerHTML =
       "Register";
     document.querySelector(
       ".login-registration__form"
     ).innerHTML += `<label class="login-registration__form__label"
                       id="confirmPasswordLabel">Confirm password</label>
                     <input type="password"
-                      class="login-registration__form__input"
-                      id="confirmPassword"/>`;
+                      class="input-div__form__input"
+                      id="confirmPassword"
+                      placeholder="Repeat password.."/>`;
     isPageLoginOrRegistration = true;
   }
 }
@@ -137,156 +140,191 @@ function openSecondPage() {
   }
 }
 
-//Generating the main page, full of api information
 function createSecondPage() {
-  body.innerHTML = `<div class="main__offers""></div>`;
-  let divToAddContentTo = document.querySelector(".main__offers");
-
+  body.innerHTML = `<div class="main__offers""></div>
+                    <div class="modal"></div>`;
+  let placeInPageToAddCode = document.querySelector(".main__offers");
   fetch("https://jsonplaceholder.typicode.com/users")
     .then(date => date.json())
-    .then(arrayOfPeopleToDisplay => {
-      arrayOfPeopleToDisplay.forEach(function(item) {
-        //Creating each person's information into an element
-        divToAddContentTo.innerHTML += `<div class ="offers__item">
-            <img class="item__img" src="img/user.jpg"/>
-            <p class="item__img-description">${item.name}</p>
-            <p class="item__paragraph">Email: ${item.email}</p>
-            <span class="item__price">City: ${item.address.city}</span>
-            <button class="userDetails" class="item__button">Details</button>
-            <button class="userPosts" class="item__button">Posts</button>
-          </div>`;
+    .then(users => {
+      users.forEach(function(user) {
+        addMainContent(user);
       });
-      setClickOnDetailButtons();
-      setClickOnPostButtons();
-      divToAddContentTo.innerHTML += `<button class="scroll-to-top"></button>`;
-      document.querySelector(".scroll-to-top")
-        .addEventListener("click", goToTopFunction);
+      users.forEach(function(user, index){
+        addDetailsPopUp(user, index);
+        addPostsPopUp(user, index);
+      });
     });
+  placeInPageToAddCode.innerHTML += `<button class="scroll-to-top" 
+    onclick="goToTop()"><i class="upArrow"></i></button>`;
 }
 
-function setClickOnDetailButtons() {
-  document.querySelectorAll(".userDetails").forEach(function(detailButton) {
-    detailButton.addEventListener("click", function() {
-      getUsersDetailsAndShowThem(
-        detailButton.parentNode.children[1].textContent
-      );
-    });
+function addMainContent(user){
+  let placeInPageToAddCode = document.querySelector(".main__offers");
+  placeInPageToAddCode.innerHTML += 
+  `<div class ="offers__item">
+    <img class="item__img" src="img/user.jpg"/>
+    <p class="item__img-description">${user.name}</p>
+    <p class="item__paragraph">Email: ${user.email}</p>
+    <span class="item__price">City: ${user.address.city}</span>
+    <button class="item__button">Posts</button>
+  </div>`;
+}
+
+function addDetailsPopUp(user, index){
+  let image = document.querySelectorAll(".item__img")[index];
+  let modal = document.querySelector(".modal");
+  image.addEventListener("click", () => {
+    modal.innerHTML += 
+    `<div class="container">
+      <div class ="modal-content">
+        <span class="close" onclick="closeModalPopUp()">&#10006;</span>
+        <h1>Name: ${user.name}</h1>
+          <p>Username:${user.username}</p>
+          <p>Email:${user.email}</p>
+          <p>Phone:${user.phone}</p>
+          <p>Web:${user.website}</p>
+        <h1>Address:</h1>
+          <p>Street:${user.address.street}</p>
+          <p>City:${user.address.city}</p>
+          <p>Zipcode:${user.address.zipcode}</p>
+          <p>lat:-${user.address.geo.lat}</p>
+          <p>lng:-${user.address.geo.lng}</p>
+        <h1>Company:</h1>
+          <p>Name:${user.company.name}</p>
+          <p>Phrase:${user.company.catchPhrase}</p>
+          <p>Bs:${user.company.bs}</p>
+      </div>
+    </div>`;
+    modal.style.display = "block";
   });
 }
 
-function setClickOnPostButtons() {
-  document.querySelectorAll(".userPosts").forEach(function(postButton) {
-    postButton.addEventListener("click", function() {
-      getUsersPostsAndShowThem(postButton.parentNode.children[1].textContent);
-    });
-  });
-}
-
-function getUsersDetailsAndShowThem(nameWeHave) {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then(date => date.json())
-    .then(arrayOfPeopleToDisplay => {
-      arrayOfPeopleToDisplay.forEach(user => {
-        if (user.name === nameWeHave) {
-          body.innerHTML += `<div class="modal">
-          <div class="container">
-            <div class ="modal-content">
-              <span class="close">&#10006;</span>
-              <h1>Name: ${user.name}</h1>
-                <p>Username:${user.username}</p>
-                <p>Email:${user.email}</p>
-                <p>Phone:${user.phone}</p>
-                <p>Web:${user.website}</p>
-              <h1>Address:</h1>
-                <p>Street:${user.address.street}</p>
-                <p>City:${user.address.city}</p>
-                <p>Zipcode:${user.address.zipcode}</p>
-                <p>lat:-${user.address.geo.lat}</p>
-                <p>lng:-${user.address.geo.lng}</p>
-              <h1>Company:</h1>
-                <p>Name:${user.company.name}</p>
-                <p>Phrase:${user.company.catchPhrase}</p>
-                <p>Bs:${user.company.bs}</p>
-            </div>
-            </div>
-          </div>`;
-          document
-            .querySelector(".close")
-            .addEventListener("click", ClosePopUp);
-        }
+function addPostsPopUp(user, index){
+  let buttons = document.querySelectorAll(".item__button")[index];
+  let modal = document.querySelector(".modal");
+  buttons.addEventListener("click", () => {
+    modal.innerHTML += 
+      `<div class="container">
+        <div class="modal-content">
+          <span class="close" onclick="closeModalPopUp()">&#10006;</span>
+          <h1>User ID:${user.id}</h1>
+        </div>
+    </div>`;
+    let basicModulDiv = document.querySelector(".container");
+    fetch("https://jsonplaceholder.typicode.com/users/1/posts")
+      .then(date => date.json())
+      .then(postsToDisplay => {
+        postsToDisplay.forEach(function(post) {
+          if (post.userId === user.id) {
+            basicModulDiv.innerHTML += 
+            `<div class="modal-content">
+              <h2>Title:${post.title}</h2>
+              <span>Post ID:${post.id}</span>
+              <p>Post:${post.body}</p>
+            </div>`;
+          }
+        });
+      })
+      .then(() => {
+        basicModulDiv.innerHTML += 
+          `<button class="modal-content__add-post-button">
+            Add new Post</button>`;
+        document.querySelector(".modal-content__add-post-button")
+          .addEventListener("click", () => newPostForm(user.id));
       });
+      modal.style.display = "block";
     });
 }
 
-function getUsersPostsAndShowThem(nameWeHave) {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then(date => date.json())
-    .then(arrayOfPeopleToDisplay => {
-      arrayOfPeopleToDisplay.forEach(user => {
-        if (user.name === nameWeHave) {
-          body.innerHTML += `<div class="modal">
-            <div class="container">
-            <div class="modal-content">
-              <h1>User ID:${user.Id}</h1>
-              <span class="close">&#10006;</span>
-            </div>
-            </div>
-          </div>`;
-          document
-            .querySelector(".close")
-            .addEventListener("click", ClosePopUp);
-          let basicModulDiv = document.querySelector(".container");
-          console.log(basicModulDiv);
-          fetch("https://jsonplaceholder.typicode.com/users/1/posts")
-            .then(date => date.json())
-            .then(arrayOfPostsToDisplay => {
-              arrayOfPostsToDisplay.forEach(function(post) {
-                if (post.userId === user.id) {
-                  basicModulDiv.innerHTML += `<div class="modal-content">
-                    <h2>Title:${post.title}</h2>
-                    <span>Post ID:${post.id}</span>
-                    <p>Post:${post.body}</p>
-                  </div>`;
-                }
-              });
-            });
-        }
-      });
-    });
+function newPostForm(id) {
+  body.innerHTML += 
+    `<div class="input-div">
+      <form class="input-div__form">
+        <span class="close" onclick="closeFormPopUp()">&#10006;</span>
+        <h1 class="input-div__title">New Post</h1><br /><br />
+        <label>Enter title:</label>
+        <input id="form-title" class="input-div__form__input" placeholder="Your title.."/>
+        <label>Enter post text:</label>
+        <textarea id="form-text" class="input-div__form__input" rows="10" cols="100" placeholder="Your text.."></textarea>
+        <button class="input-div__form__button">Submit</button>
+      </form>
+    </div>`;
+  
+  document.querySelector(".input-div__form__button")
+    .addEventListener("click", () => {
+    let post = {
+      title: document.querySelector("#form-title").value,
+      text: document.querySelector("#form-text").value,
+      userId : id
+    }
+    if(CheckIfPostOkay(post)){
+      createPost(post);
+    }
+  })
 }
 
-function ClosePopUp() {
-  body.removeChild(document.querySelector(".modal"));
+function CheckIfPostOkay(post) {
+  if(post.title !== "" && post.text !== ""){ 
+    return true; }
+  return false; 
 }
-createFirstPage();
+
+function createPost(post) {
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+        title: post.title,
+        text: post.text,
+        userId: post.userId
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(alert("successfully created post"))
+}
+
+function closeModalPopUp() {
+  let element = document.querySelector(".modal");
+  element.style.display = "none";
+  element.innerHTML = "";
+}
+
+function closeFormPopUp(){
+
+}
+
 //If the local storage is empty then the first page must load as a register page
 //Othervise it loads as a login page
 if (localStorage.length !== 0) {
   login();
 }
 //event listeners
-document.querySelector("#login")
-  .addEventListener("click", login);
-document.querySelector("#register")
-  .addEventListener("click", register);
+document.querySelector("#login").addEventListener("click", login);
+document.querySelector("#register").addEventListener("click", register);
 document
-  .querySelector(".login-registration__button")
+  .querySelector(".input-div__form__button")
   .addEventListener("click", openSecondPage);
 
 //Button for scrolling to the top of the page
 window.onscroll = function() {
-  scrollFunction();
+  scroll();
 };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+function scroll() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
     document.querySelector(".scroll-to-top").style.display = "block";
   } else {
     document.querySelector(".scroll-to-top").style.display = "none";
   }
 }
 
-function goToTopFunction() {
+function goToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
